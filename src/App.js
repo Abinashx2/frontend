2aa07678633535bd8ec9a690678340b7220a3a0a
Abinashx5 <abinashdaimari5857@gmail.com>
2025-08-14 10:21:48 +0530

    adding google analytics

--- src/App.js
+++ src/App.js
@@ -1,63 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Post from './Post';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Create from './components/Create';
-import Login from './Pages/Login'
+import Login from './Pages/Login';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-// import Bpost from './components/Bpost.js';
-// import Bpost from '../components/Bpost';
 import Bpost from './components/Bpost';
-// import AdminDashboard from './Pages/Dashboard';
 import Dashboard from './Pages/Dashboard';
 import ProtectedRoute from './Pages/Protected';
 import EditPost from './Pages/Editpost';
 import Header from './components/Header';
 import About from './components/About';
 import Contact from './components/Contact';
-// import BlogList from './components/Bloglist';
 import Bloglist from './components/Bloglist';
 
+// Main content component with tracking
+const TrackedAppContent = () => {
+  const location = useLocation();
 
-const App = () => {
+  useEffect(() => {
+    if (typeof window.gtag === 'function') {
+      window.gtag('event', 'page_view', {
+        page_path: location.pathname + location.search,
+        page_title: document.title,
+      });
+    }
+  }, [location]);
+
   return (
-    <>
     <div className='container'>
-      {/* <h1 className='header'>Bodoland Times</h1> */}
-      <Header></Header>
-      
-      <Router>
-      <Navbar></Navbar>
+      <Header />
+      <Navbar />
       <main className='main-content'>
         <Routes>
-        
-          
-          <Route path="/" element={<Post></Post>}></Route>
-          <Route path='/blog/:id' element={<Bpost></Bpost>}></Route>
-          
-          <Route path='/Login' element={<Login></Login>}></Route>
+          <Route path="/" element={<Post />} />
+          <Route path='/blog/:id' element={<Bpost />} />
+          <Route path='/Login' element={<Login />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact></Contact>} />
-          <Route path="/others" element={<Bloglist></Bloglist>} />
-          <Route element={<ProtectedRoute></ProtectedRoute>}>
-            <Route path='/admin/dashboard' element={<Dashboard></Dashboard>}></Route>
-            <Route path='/blog/create' element={<Create></Create>}></Route>
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/others" element={<Bloglist />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path='/admin/dashboard' element={<Dashboard />} />
+            <Route path='/blog/create' element={<Create />} />
             <Route path="/blog/:id/edit" element={<EditPost />} />
-            
           </Route>
         </Routes>
-        </main>
-      </Router>
-      {/* <Post></Post> */}
-      {/* <Bpost id={1}></Bpost> */}
-      
-      
-      <Footer></Footer>
-      
+      </main>
+      <Footer />
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default App
+// Main App component with Router
+const App = () => {
+  return (
+    <Router>
+      <TrackedAppContent />
+    </Router>
+  );
+};
+
+export default App;
